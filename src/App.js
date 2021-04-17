f32b550918dd62f757b2f1f35515b4a3027591c9
Natsuno Tsukuda <natsuno-t@ws20is007.naist.jp>
2021-04-17 23:37:19 +0900

    デザイン調整

--- src/App.js
+++ src/App.js
@@ -1,27 +1,109 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { useEffect, useState } from "react";
 import DeckGL from "@deck.gl/react";
-import { PolygonLayer } from "@deck.gl/layers";
+import { StaticMap } from 'react-map-gl';
+import { GeoJsonLayer } from "@deck.gl/layers";
+import { Slider, Tooltip } from '@material-ui/core';
+
+const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoibmF0c3Vub3RzdWt1ZGEiLCJhIjoiY2tua2l1ZzB0MGJqaDJucDNqYnd2MmVlZyJ9.n2deVJQiKF_O3D8TGX8E9Q';
 
 function App() {
-  const [yyyymmdd, setYYYYMMDD] = useState("20210110");
   const [perDayData, setPerDayData] = useState({});
+  const [prefPolygon, setPrefPolygon] = useState({})
+  const [yyyymmdd, setYYYYMMDD] = useState("2021-01-10");
+  const [dateArray, setDateArray] = useState([])
+  const [dateSliderValue, setDateSliderValue] = useState(0)
+  const [viewState, setViewState] = useState({
+    longitude: 135,
+    latitude: 38,
+    zoom: 4.6,
+    pitch: 45
+  })
+
+  //settings for deck.gl
+  const geoLayer = new GeoJsonLayer({
+    id: 'geojson-layer',
+    data: prefPolygon,
+    wireframe: true,
+    filled: true,
+    stroked: true,
+    opacity: 0.8,
+    pickable: true,
+    filled: true,
+    extruded: true,
+    lineWidthScale: 20,
+    lineWidthMinPixels: 2,
+    getFillColor: [240, 68, 47, 180],
+    getRadius: 100,
+    getLineWidth: 1,
+    getElevation: f => perDayData[yyyymmdd][f.properties.name]['per_day'] * 100
+  });
+
   useEffect(() => {
     console.log("mounted");
     async function fetchData() {
-      await loadDayData();
+      const [dayData, dayArray] = await loadDayData();
+      console.log(dayArray)
+      setPerDayData(dayData)
+      setDateArray(dayArray)
+      setDateSliderValue(0)
+      setYYYYMMDD(dayArray[0])
+      
+      const polygonData = await loadPrefPolygon();
+      setPrefPolygon(polygonData)
     }
     fetchData();
     return () => console.log("unmounting...");
   }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <DeckGL
-          viewState={viewState}
-        />
-      </header>
+    <div style={{
+      height: "100vh",
+      width: "100vw",
+      display: "flex"
+    }}
+    >
+      <DeckGL
+        layers={[geoLayer]}
+        initialViewState={viewState}
+        controller={true}
+        style={{
+          backgroundColor: "white",
+          width: "100%",
+          height: "100%",
+        }}
+      >
+        <StaticMap mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN} />
+      </DeckGL>
+      <Slider
+        defaultValue={dateArray.length-1}
+        ValueLabelComponent={props => ValueLabelComponent(props, yyyymmdd)}
+        valueLabelDisplay="on"
+        step={1}
+        marks
+        min={0}
+        max={dateArray.length-1}
+        style={{
+          position: "absolute",
+          bottom: "25px",
+          width: "90vw",
+          marginLeft: "5vw"
+        }}
+        onChange={(e, v)=>setYYYYMMDD(dateArray[v])}
+      />
+      {/*<Slider
+          value={dateSliderValue}
+          aria-labelledby="discrete-slider"
+          ValueLabelComponent={props => ValueLabelComponent(props, yyyymmdd)}
+          valueLabelDisplay="on"
+          step={1}
+          marks
+          min={0}
+          max={(dateArray).length-1}
+          onChange={value=>{
+            setYYYYMMDD(dateArray[value])
+          }}
+          style={{bottom: 10, position: 'absolute', right: 10, left: 10}}
+        />*/}
     </div>
   );
 }
@@ -34,8 +116,39 @@ async function loadDayData() {
     },
   })
     .then((res) => res.json())
-    .then((resJson) => console.log(resJson));
-  return jsonData;
+    .then((resJson) => { return resJson });
+  return [jsonData, Object.keys(jsonData)];
+}
+
+async function loadPrefPolygon() {
+  const jsonData = await fetch("jsonData/prefectures.json", {
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+  })
+    .then((res) => res.json())
+    .then((resJson) => {
+      const polyData = resJson['features']
+      return polyData
+    });
+  return { type: "FeatureCollection", features: jsonData };
+}
+
+function ValueLabelComponent(props, date) {
+  const { children, open, value } = props;
+  return (
+    <Tooltip open={open} enterTouchDelay={0} placement="top" title={date}>
+      {children}
+    </Tooltip>
+  );
+}
+
+function getHeight(data) {
+}
+
+function getColor(data) {
+
 }
 
 export default App;
